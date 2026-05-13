32a9579f563e16bb0a088fa2b4ab351b8445a5e1
Betterbird <betterbird@betterbird.eu>
2026-05-13 22:31:30 +0200

    [153] Add 22 MOZ patches (2 missing).

---
+++ 153/153.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
+MOZILLA_REV=4201ad8bbb183a6ae00df4cfd5d775a976564828
+COMM_REPO=https://hg.mozilla.org/comm-central/
+COMM_REV=cadc38913d17d7b46fa8cb715bc5aa1f01751791
+RUST_VER=1.86.0
+MOZ="-moz"
+MOZU="-moz"
