95bf4a8d1839027ff3d94479ce666817bdd028f4
Betterbird <betterbird@betterbird.eu>
2026-05-15 21:22:33 +0200

    [140] Prepare for 140.11.0esr-bb23: rebase one patch.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
-MOZILLA_REV=a24e9e115e02794f72dab9ef6081244403b0183a
+MOZILLA_REV=2e36c464a92f1942683abbed6ceb442308db5eb0
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=90a6ec3c80165639181fec5128fd24fa20e41021
+COMM_REV=81aede69c16b80937e10ff623edd6ad327239d65
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
